ca658f7ed6ce5069f4b69acbf8dad167ebfd6385
brunocoder <brunocoder@yahoo.com>
2017-01-06 00:01:24 -0800

    bug fixed, package files updated

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
+var mongo = require('mongodb');
+var db = require('monk')('localhost/blog-nodejs');
+/* Homepage Blog Posts */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Members' });
+  var db = req.db;
+  var posts =  db.get('posts');
+  posts.find({},{},function(err,posts){
+    res.render('index',{
+      'posts':posts
+    })
+  });
 });
 
 module.exports = router;
